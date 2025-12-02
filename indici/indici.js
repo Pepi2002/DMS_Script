5667650ea6ee1b67efba0f1cc177057cd42a1f50
aaaaaaurora <151461184+aaaaaaurora@users.noreply.github.com>
2025-12-02 13:25:04 +0100

    Add files via upload

---
+++ indici/indici.js
@@ -0,0 +1,40 @@
+// --- 1. Collezione: nutrients ---
+db.nutrients.createIndex(
+  { "id": 1 }, 
+  { unique: true }
+);
+db.nutrients.createIndex(
+  { "name": 1 }
+);
+// --- 2. Collezione: measure_units ---
+db.measure_units.createIndex(
+  { "id": 1 }, 
+  { unique: true }
+);
+db.measure_units.createIndex(
+  { "name": 1 }
+);
+// --- 3. Collezione: foods ---
+db.foods.createIndex(
+  { "fdc_id": 1 }, 
+  { unique: true }
+);
+db.foods.createIndex(
+  { "portions.measure_unit_ref": 1 }
+);
+// --- 4. Collezione: conversion_factors ---
+db.conversion_factors.createIndex(
+  { "nutrient_factor_id_": 1 }, 
+  { unique: true }
+);
+db.conversion_factors.createIndex(
+  { "fdc_id": 1 }
+);
+// --- 5. Collezione: ingredients ---
+db.ingredients.createIndex(
+  { "food_ref": 1, "seq_num": 1 }, 
+  { unique: true }
+);
+db.ingredients.createIndex(
+  { "nutritional_profile.nutrient_ref": 1 }
+);
