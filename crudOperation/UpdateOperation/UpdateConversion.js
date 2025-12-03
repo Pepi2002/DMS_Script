b5d5f5082a6d1307386cc157297c7565a288f160
Pepi <83947531+Pepi2002@users.noreply.github.com>
2025-12-03 17:47:51 +0100

    Update UpdateConversion.js

--- crudOperation/UpdateOperation/UpdateConversion.js
+++ crudOperation/UpdateOperation/UpdateConversion.js
@@ -1,22 +1,21 @@
-/* === UPDATE: COLLEZIONE conversion_factor === */
-
-// SETUP-4: Inserimento Dati di Test (nutrient_ref: 203)
+// SETUP-4: Inserimento Dati di Test (nutrient_ref: 5000000)
 db.conversion_factors.insertOne({
-    nutrient_factor_id_: 203,
+    nutrient_factor_id_: 5000000,
     fdc_id: 900010,
     protein_factor: {
         value: 5.75
-    } // Fattore iniziale
+    } 
 });
 
-// SUCCESS-4: Aggiorna il valore di protein_factor (nutrient_ref: 203)
+// TEST SUCCESS-4: Aggiorna il valore di protein_factor (nutrient_ref: 5000000)
 db.conversion_factors.updateOne(
-    { nutrient_factor_id_: 203 },
+    { nutrient_factor_id_: 5000000 },
     { $set: { "protein_factor.value": 6.5 } }
 );
 
-// FAILURE-4: Tenta di impostare una stringa in un campo numerico (Risultato atteso: ValidationError)
+// TEST FAILURE-4: Tenta di impostare una stringa in un campo numerico 
+// Risultato atteso: Document failed validation
 db.conversion_factors.updateOne(
-    { nutrient_factor_id_: 203 },
+    { nutrient_factor_id_: 5000000 },
     { $set: { protein_factor: "sei_virgola_cinque" } }
 );
