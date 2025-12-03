3b98d36deba9094761d46dd9564b3de6e441baa3
Pepi <83947531+Pepi2002@users.noreply.github.com>
2025-12-03 17:30:27 +0100

    Update ReadConversion.js

--- crudOperation/ReadOperation/ReadConversion.js
+++ crudOperation/ReadOperation/ReadConversion.js
@@ -1,22 +1,18 @@
-/* === READ: COLLEZIONE conversion_factor - SETUP === */
-
-// SETUP-4: Inserimento Dati di Test (con valore > 6.0)
+// SETUP-4: Inserimento Dati di Test
 db.conversion_factors.insertOne({
     fdc_id: 9000010,
     nutrient_factor_id_: 203,
     protein_factor: { 
-        value: 6.75 // Valore alto per READ-E4
+        value: 1000000000000 // Valore alto per HIGH-4
     } 
 });
 
-/* === READ: COLLEZIONE conversion_factor - QUERY === */
-
-// READ-D4: Lettura Diretta per ID (fdc_id: 900010)
+// TEST ID-4: Lettura Diretta per ID (fdc_id: 900010)
 db.conversion_factors.find(
     { fdc_id: 9000010 }
-).limit(1).pretty();
+).pretty();
 
-// READ-E4: Query su dati incorporati (Filtra per protein_factor.value > 6.0)
+// TEST HIGH-4: Query su dati incorporati (Filtra per protein_factor.value > 900000000000)
 db.conversion_factors.find(
-    { "protein_factor.value": { $gt: 6.0 } }
-).limit(5).pretty();
+    { "protein_factor.value": { $gt: 900000000000 } }
+).pretty();
