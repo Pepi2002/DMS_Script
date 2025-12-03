14958d895bcbee072efc7a26989e08bb8746ac2b
Pepi <83947531+Pepi2002@users.noreply.github.com>
2025-12-03 17:35:06 +0100

    Update ReadMeasureUnit.js

--- crudOperation/ReadOperation/ReadMeasureUnit.js
+++ crudOperation/ReadOperation/ReadMeasureUnit.js
@@ -1,19 +1,15 @@
-/* === READ: COLLEZIONE measure_unit - SETUP === */
-
 // SETUP-5: Inserimento Dati di Test
 db.measure_unit.insertOne({
     id: 1000,
     name: "Test Cup",
 });
 
-/* === READ: COLLEZIONE measure_unit - QUERY === */
-
-// READ-D5: Lettura Diretta per ID (id: 1000)
+// TEST ID-5: Lettura Diretta per ID (id: 1000)
 db.measure_unit.find(
     { id: 1000 }
-).limit(1).pretty();
+).pretty();
 
-// READ-E5: Query per attributo (Filtra per abbreviation: 'tz')
+// TEST HIGH-5: Query per attributo (Filtra per name: 'Test Cup')
 db.measure_unit.find(
     { name: "Test Cup" }
-).limit(5).pretty();
+).pretty();
