8b6ae5304d34eaf76ec4edbb20e1ed538350e65f
Pepi <83947531+Pepi2002@users.noreply.github.com>
2025-12-03 17:51:29 +0100

    Fix collection name from measure_unit to measure_units

--- crudOperation/ReadOperation/ReadMeasureUnit.js
+++ crudOperation/ReadOperation/ReadMeasureUnit.js
@@ -1,15 +1,16 @@
 // SETUP-5: Inserimento Dati di Test
-db.measure_unit.insertOne({
+db.measure_units.insertOne({
     id: 1000,
     name: "Test Cup",
 });
 
 // TEST ID-5: Lettura Diretta per ID (id: 1000)
-db.measure_unit.find(
+db.measure_units.find(
     { id: 1000 }
 ).pretty();
 
 // TEST HIGH-5: Query per attributo (Filtra per name: 'Test Cup')
-db.measure_unit.find(
+db.measure_units.find(
     { name: "Test Cup" }
 ).pretty();
+
