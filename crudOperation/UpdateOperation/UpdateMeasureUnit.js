e061007876caa4408288e9615b0707e14ad23c27
Pepi <83947531+Pepi2002@users.noreply.github.com>
2025-12-03 17:49:49 +0100

    Update database collection name from measure_unit to measure_units

--- crudOperation/UpdateOperation/UpdateMeasureUnit.js
+++ crudOperation/UpdateOperation/UpdateMeasureUnit.js
@@ -1,18 +1,19 @@
 // SETUP-5: Inserimento Dati di Test (id: 1000)
-db.measure_unit.insertOne({
+db.measure_units.insertOne({
     id: 1000,
     name: "Test Spoon",
 });
 
 // TEST SUCCESS-5: Aggiorna l'abbreviazione dell'unità (id: 1000)
-db.measure_unit.updateOne(
+db.measure_units.updateOne(
     { id: 1000 },
     { $set: { name: "Test Cup" } }
 );
 
 // TEST FAILURE-5: Tenta di impostare una stringa nel campo ID numerico 
 // Risultato atteso: Document failed validation
-db.measure_unit.updateOne(
+db.measure_units.updateOne(
     { id: 1000 },
     { $set: { id: "nuovo_id" } }
 );
+
