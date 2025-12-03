97d0eecdb10aac3e534e770c1a7f37d7849e52f6
Pepi <83947531+Pepi2002@users.noreply.github.com>
2025-12-03 17:07:50 +0100

    Update ReadFood.js

--- crudOperation/ReadOperation/ReadFood.js
+++ crudOperation/ReadOperation/ReadFood.js
@@ -1,43 +1,39 @@
-/* === READ: COLLEZIONE foods - SETUP === */
-
 // SETUP-1: Inserimento Dati di Test (Due documenti con modifier 'cup')
 db.foods.insertMany([
     {
         fdc_id: 9000010, 
-        description: "Cibo base test", // Filtro comune
+        description: "Cibo base test", // Per usare solo i dati di test
         data_type: "sr_legacy_food",
         publication_date: ISODate("2024-11-01T00:00:00Z"), 
         acquisitions: [],
-        survey_data: { start_date: null, end_date: null }, 
+        survey_data: {}, 
         portions: [{ 
             gram_weight: 100.3, 
             measure_unit_ref: 1000, 
-            modifier: "cup" 
+            modifier: "cup" // Filtro comune
         }]
     },
     {
         fdc_id: 9000011, 
-        description: "Cibo base test", // Filtro comune
+        description: "Cibo base test", // Per usare solo i dati di test
         data_type: "sr_legacy_food",
         publication_date: ISODate("2024-11-01T00:00:00Z"), 
         acquisitions: [],
-        survey_data: { start_date: null, end_date: null }, 
+        survey_data: {}, 
         portions: [{ 
             gram_weight: 100.3, 
             measure_unit_ref: 1000, 
-            modifier: "cup" 
+            modifier: "cup" // Filtro comune
         }]
     }
 ]);
 
-/* === READ: COLLEZIONE foods - QUERY === */
-
-// READ-D1: Lettura Diretta per ID (fdc_id: 900010)
+// TEST ID-1: Lettura Diretta per ID (fdc_id: 900010)
 db.foods.find(
     { fdc_id: 9000010 }
-).limit(1).pretty();
+).pretty();
 
-// READ-E1: Query su dati incorporati (Filtra per modifier 'cup' all'interno di portions)
+// TEST HIGH-1: Query su dati incorporati (Filtra per modifier 'cup' all'interno di portions)
 db.foods.find(
     { $and: [{description: "Cibo base test"}, {"portions.modifier": "cup" }]}
-).limit(5).pretty();
+).pretty();
