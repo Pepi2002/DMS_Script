a64e0adf7f6833854f6c4a1ccb1ad3cc02d1609d
Pepi <83947531+Pepi2002@users.noreply.github.com>
2025-12-03 17:31:16 +0100

    Update comments and test queries in ReadNutrients.js

--- crudOperation/ReadOperation/ReadNutrients.js
+++ crudOperation/ReadOperation/ReadNutrients.js
@@ -4,12 +4,13 @@ db.nutrients.insertMany([
     { id: 9000002, nutrient_nbr: 301, name: "Calcio", unit_name: "MG", rank: 10000000 }  // Rank comune
 ]);
 
-// ID-3: Lettura Diretta per ID (id:  9000001)
+// TEST ID-3: Lettura Diretta per ID (id:  9000001)
 db.nutrients.find(
     { id:  9000001 }
 ).pretty();
 
-// HIGH-3: Query per attributo (Filtra per rank: 10000000)
+// TEST HIGH-3: Query per attributo (Filtra per rank: 10000000)
 db.nutrients.find(
     { rank: 10000000 }
 ).pretty();
+
