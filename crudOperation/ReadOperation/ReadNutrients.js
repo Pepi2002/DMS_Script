ae157dccb7044d9a7358144f71ed10e6700648f3
Pepi <83947531+Pepi2002@users.noreply.github.com>
2025-12-03 17:26:55 +0100

    Update nutrient data and queries in ReadNutrients.js

--- crudOperation/ReadOperation/ReadNutrients.js
+++ crudOperation/ReadOperation/ReadNutrients.js
@@ -1,19 +1,15 @@
-/* === READ: COLLEZIONE nutrients - SETUP === */
-
 // SETUP-3: Inserimento Dati di Test
 db.nutrients.insertMany([
-    { id: 9000001, nutrient_nbr: 203, name: "Proteine", unit_name: "G", rank: 100 }, // Rank comune
-    { id: 9000002, nutrient_nbr: 301, name: "Calcio", unit_name: "MG", rank: 100 }  // Rank comune
+    { id: 9000001, nutrient_nbr: 203, name: "Proteine", unit_name: "G", rank: 10000000 }, // Rank comune
+    { id: 9000002, nutrient_nbr: 301, name: "Calcio", unit_name: "MG", rank: 10000000 }  // Rank comune
 ]);
 
-/* === READ: COLLEZIONE nutrients - QUERY === */
-
-// READ-D3: Lettura Diretta per ID (nutrient_nbr: 203)
+// ID-3: Lettura Diretta per ID (id:  9000001)
 db.nutrients.find(
     { id:  9000001 }
-).limit(1).pretty();
+).pretty();
 
-// READ-E3: Query per attributo (Filtra per unit_name: 'G')
+// HIGH-3: Query per attributo (Filtra per rank: 10000000)
 db.nutrients.find(
-    { unit_name: "G" }
-).limit(5).pretty();
+    { rank: 10000000 }
+).pretty();
