2492603e7abf99f85c0b9853bbd342caf5f95d32
Pepi <83947531+Pepi2002@users.noreply.github.com>
2025-12-03 17:48:49 +0100

    Refactor comments and update test descriptions

--- crudOperation/UpdateOperation/UpdateMeasureUnit.js
+++ crudOperation/UpdateOperation/UpdateMeasureUnit.js
@@ -1,18 +1,17 @@
-/* === UPDATE: COLLEZIONE measure_unit === */
-
 // SETUP-5: Inserimento Dati di Test (id: 1000)
 db.measure_unit.insertOne({
     id: 1000,
     name: "Test Spoon",
 });
 
-// SUCCESS-5: Aggiorna l'abbreviazione dell'unità (id: 1000)
+// TEST SUCCESS-5: Aggiorna l'abbreviazione dell'unità (id: 1000)
 db.measure_unit.updateOne(
     { id: 1000 },
     { $set: { name: "Test Cup" } }
 );
 
-// FAILURE-5: Tenta di impostare una stringa nel campo ID numerico (Risultato atteso: ValidationError)
+// TEST FAILURE-5: Tenta di impostare una stringa nel campo ID numerico 
+// Risultato atteso: Document failed validation
 db.measure_unit.updateOne(
     { id: 1000 },
     { $set: { id: "nuovo_id" } }
